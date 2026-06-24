39dfe7d9c48f7b8154c537eb5821a6ac22c2ee53
lawky0515-cpu <lawky0515@gmail.com>
2026-06-24 21:52:11 +0800

    Update image path for Yoga Mat product

--- products.js
+++ products.js
@@ -40,7 +40,7 @@ const products = [
         price: 19.99,
         category: "sports",
         desc: "6mm Non Slip Yoga Mat",
-        image: "https://gw.alicdn.com/bao/uploaded/i1/2577160010/O1CN01UgTVl71Bwd0wmWynL_!!2577160010.jpg_.webp",
+        image: "images/EAC95464-77D7-4B20-8EA0-578230FF9E1E.png",
         colors: [
             { name: "Pink", code: "#ec4899" },
             { name: "Blue", code: "#3b82f6" }
